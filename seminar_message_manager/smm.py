1914b72b9f8914d5c179f4112f0e466293a23f37
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2025-10-21 15:49:25 +0200

    fix send args

--- seminar_message_manager/smm.py
+++ seminar_message_manager/smm.py
@@ -92,40 +92,68 @@ def cli():
     return args
 
 
+def file_exists(filepath):
+    try:
+        with open(filepath, "r"):
+            return True
+    except FileNotFoundError:
+        return False
+
+
 def main():
     args = cli()
     if args.send:
-        args.send_mail = True
-        args.send_zulip = True
-        args.send_discord = True
+        args.send_mail = file_exists(args.template_mail)
+        args.send_zulip = file_exists(args.template_zulip)
+        args.send_discord = file_exists(args.template_discord)
+        args.send_mattermost = file_exists(args.template_mattermost)
 
-    if args.template_mail and args.send_mail:
+    if file_exists(args.template_mail) and args.send_mail:
         mail = parse_annoucement(args.date, args.seminar_csv, args.template_mail)
         print(mail)
-    if args.template_zulip and args.send_zulip:
+    if file_exists(args.template_zulip) and args.send_zulip:
         zulip_msg = parse_annoucement(args.date, args.seminar_csv, args.template_zulip)
         print(zulip_msg)
-    if args.template_discord and args.send_discord:
+    if file_exists(args.template_discord) and args.send_discord:
         discord_msg = parse_annoucement(
             args.date, args.seminar_csv, args.template_discord
         )
         print(discord_msg)
-    if args.template_mattermost and args.send_mattermost:
+    if file_exists(args.template_mattermost) and args.send_mattermost:
         mattermost_msg = parse_annoucement(
             args.date, args.seminar_csv, args.template_mattermost
         )
         print(mattermost_msg)
 
-    if args.send_mail and args.template_mail and args.mail_json:
+    if (
+        args.send_mail
+        and file_exists(args.template_mail)
+        and file_exists(args.mail_json)
+    ):
         send_email(args.mail_json, mail)
         print("\n\033[1;32mMail sent\033[0m\n")
-    if args.send_zulip and args.template_zulip and args.zulip_json:
+
+    if (
+        args.send_zulip
+        and file_exists(args.template_zulip)
+        and file_exists(args.zulip_json)
+    ):
         move_old_messages_zulip(args.zulip_json)
         send_message_to_zulip(args.zulip_json, zulip_msg)
         print("\n\033[1;32mZulip message sent\033[0m\n")
-    if args.send_discord and args.template_discord and args.discord_json:
+
+    if (
+        args.send_discord
+        and file_exists(args.template_discord)
+        and file_exists(args.discord_json)
+    ):
         send_message_to_discord(args.discord_json, discord_msg)
         print("\n\033[1;32mDiscord message sent\033[0m\n")
-    if args.send_mattermost and args.template_mattermost and args.mattermost_json:
+
+    if (
+        args.send_mattermost
+        and file_exists(args.template_mattermost)
+        and file_exists(args.mattermost_json)
+    ):
         send_message_to_mattermost(args.mattermost_json, mattermost_msg)
         print("\n\033[1;32mMattermost message sent\033[0m\n")
